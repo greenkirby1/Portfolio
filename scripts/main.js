b3ea13ee533b3d717694949af3a36981401523e7
Cassie <cassie.lee1023@gmail.com>
2024-06-24 23:43:09 +0100

    hero animation logic done and tidied up css

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
 
-// nav-bar animation
+// * nav-bar animation
 ScrollTrigger.create({
   start: 'top -80',
   end: 99999,
@@ -11,19 +11,19 @@ ScrollTrigger.create({
   }
 })
 
-const navLinks = gsap.utils.toArray('nav a')
 
-console.log(navLinks)
-
-// hero image animation
+// * hero image animation
 const tl = gsap.timeline()
 
-tl.to('.transition', {
+// const layers = gsap.utils.toArray('.layer')
+// console.log(layers)
+
+tl
+  .to('.transition', {
     scrollTrigger: {
       trigger: '.hero',
       start: 'top top',
       end: 'bottom center',
-      // markers: true,
       pin: true,
       scrub: 0.8
     },
@@ -35,9 +35,52 @@ tl.to('.transition', {
       trigger: '.hero-image',
       start: 'bottom center',
       end: '+=80%',
-      markers: true,
       scrub: 0.8,
     },
     duration: 5,
     opacity: 0,
   })
+
+const transitionTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.transition',
+    start: 'top 500px',
+    end: 'bottom center',
+    scrub: true,
+  }
+})
+
+transitionTl
+  .to('.matrix-3', {
+    yPercent: 25,
+    ease: 'power1.out'
+  })
+  .to('.matrix-2', {
+    yPercent: 15,
+    ease: 'power1.out'
+  })
+
+
+
+// * navigation animation
+const navBtns = gsap.utils.toArray('nav button')
+
+console.log(navBtns)
+
+navBtns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    console.log(e.target)
+    if (e.target.value === 'hero') {
+      tl.to('.transition', {
+
+      })
+    }
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: {
+        y: `.${e.target.value}`,
+        offsetY: () => e.target.value === 'hero' ? 'top top' : 50
+      }
+    })
+  })
+})
