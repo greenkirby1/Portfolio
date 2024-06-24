11ca154b761564df6cc8cce8c3812795b04a8caf
Cassie <cassie.lee1023@gmail.com>
2024-06-24 16:08:32 +0100

    hero image animation done

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,43 @@
-document.addEventListener('DOMContentLoaded', (e) => {
-  gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
-  ScrollTrigger.addEventListener('scrollStart', () => {
-    ScrollTrigger.create({
-      start: 'top -80',
-      end: 99999,
-      toggleClass: {
-        className: 'top-bar--scrolled',
-        targets: '.top-bar'
-      }
-    })
-  })
 
-  // ScrollTrigger.addEventListener('scrollEnd', () => {
-  //   const navbar = document.querySelector('.top-bar')
-  //   navbar.classList.remove('top-bar--scrolled')
-  // })
+// nav-bar animation
+ScrollTrigger.create({
+  start: 'top -80',
+  end: 99999,
+  toggleClass: {
+    className: 'top-bar--scrolled',
+    targets: '.top-bar'
+  }
 })
+
+const navLinks = gsap.utils.toArray('nav a')
+
+console.log(navLinks)
+
+// hero image animation
+const tl = gsap.timeline()
+
+tl.to('.transition', {
+    scrollTrigger: {
+      trigger: '.hero',
+      start: 'top top',
+      end: 'bottom center',
+      // markers: true,
+      pin: true,
+      scrub: 0.8
+    },
+    yPercent: -100,
+    ease: 'none',
+  })
+  .to('.hero-image', {
+    scrollTrigger: {
+      trigger: '.hero-image',
+      start: 'bottom center',
+      end: '+=80%',
+      markers: true,
+      scrub: 0.8,
+    },
+    duration: 5,
+    opacity: 0,
+  })
